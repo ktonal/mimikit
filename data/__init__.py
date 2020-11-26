7ab51493c877d8310bb28f113513c905c59a8c28
antoine <antoine@antoine>
2020-11-26 15:01:07 +0100

    wip

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
-from .api import Database
+from .api import Database, FeatureProxy
 from .factory import make_root_db
-from .load import load
+# from .load import load
 from .metadata import Metadata
