1e0e654c084100d9f6c0b96d5143ff4d785b93db
antoine <antoine@antoine>
2020-12-02 02:53:49 +0100

    add .kit

---
+++ kit/get_trainer.py
@@ -0,0 +1,70 @@
+import os
+import torch
+from pytorch_lightning.loggers import NeptuneLogger
+from .pl_loggers import MMKDefaultLogger
+from .pl_checkpoint import MMKCheckpoint
+from .pl_callbacks import EpochProgressBarCallback
+
+
+def get_trainer(root_dir=None,
+                version=None,
+                resume_from_checkpoint=None,
+                epochs=None,  # add MMKCheckpoint if not None
+                neptune_api_token=None,  # add NeptuneLogger if not None
+                neptune_project=None,
+                **kwargs):
+
+    # Figure out the root_dir
+    if root_dir is None:
+        if resume_from_checkpoint is not None:
+            # keep the same root_dir as the checkpoint
+            root_dir = os.path.split(os.path.dirname(resume_from_checkpoint))[0]
+        else:
+            root_dir = "./"
+
+    # Figure out the version
+    if version is not None:
+        version = int(version)
+        if version == -1:
+            versions = [int(v.split("_")[1]) for v in os.listdir(root_dir) if "version" in v]
+            next_version = 1 + (max(versions) if any(versions) else -1)
+        else:
+            next_version = version  # this overwrites any existing files!
+        default_root_dir = os.path.join(root_dir, "version_" + str(next_version))
+    else:
+        next_version = 0
+        default_root_dir = root_dir
+
+    # Figure out loggers
+    if (neptune_api_token is None) != (neptune_project is None):
+        raise ValueError("Expected `neptune_project` and `neptune_api_token` to both be either None or not None")
+
+    user_logger = kwargs.get("logger", None)
+    loggers = []
+
+    if neptune_api_token is not None:
+        loggers.append(NeptuneLogger(neptune_api_token, neptune_project))
+    if user_logger is None:
+        loggers.append(MMKDefaultLogger(default_root_dir, next_version))
+    elif user_logger:
+        loggers.append(user_logger)
+    else: # it is falsy and the user DOESN'T want logging
+        loggers = False
+
+    kwargs["logger"] = loggers
+
+    # Figure out checkpoints
+    # if the user specifies one, we don't add ours
+    if not kwargs.get("checkpoint_callback", False):
+        if epochs is not None:
+            kwargs.setdefault("checkpoint_callback", MMKCheckpoint(dirpath=default_root_dir, epochs=epochs))
+
+    # add epoch progress bar
+    kwargs.setdefault("callbacks", []).append(EpochProgressBarCallback())
+    kwargs.setdefault("progress_bar_refresh_rate", 5)
+    kwargs.setdefault("process_position", 1)
+
+    # Figure out gpus (with tpus one would probably have to set gpus=None)
+    kwargs.setdefault("gpus", torch.cuda.device_count())
+
+    return default_root_dir, next_version, kwargs
---
+++ kit/pl_hooks.py
@@ -0,0 +1,126 @@
+from pytorch_lightning.utilities.cloud_io import load as pl_load
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from time import time, gmtime
+from typing import Optional, Union, Callable, Dict, IO
+import torch
+import os
+
+
+class EpochEndPrintHook:
+
+    # unfortunately this can not be a callback since we need the training_step's output...
+
+    def on_epoch_start(self):
+        # convenience to have printing of the loss at the end of the epoch
+        self._ep_metrics = {}
+        self._batch_count = 0
+
+    def training_step_end(self, out):
+        for metric, val in out.items():
+            if metric not in self._ep_metrics:
+                self._ep_metrics.setdefault(metric, val.detach())
+            else:
+                self._ep_metrics[metric] += val.detach()
+        self._batch_count += 1
+        return out
+
+    def on_epoch_end(self):
+        to_print = "Epoch %i " % self.current_epoch
+        for k, v in self._ep_metrics.items():
+            to_print += "- %s :%.4f " % (k, v / self._batch_count)
+        self.print(to_print)
+
+    def on_fit_start(self):
+        self._ep_time = time()
+
+    def on_fit_end(self):
+        total_time = gmtime(time() - self._ep_time)
+        self.print("Training finished after "
+                   "{0} days {1} hours {2} mins {3} seconds".format(total_time[2] - 1, total_time[3],
+                                                                    total_time[4], total_time[5]))
+
+
+class MMKHooks:
+
+    @classmethod
+    def load_from_checkpoint(
+            cls,
+            checkpoint_path: Union[str, IO],
+            map_location: Optional[Union[Dict[str, str], str, torch.device, int, Callable]] = None,
+            strict: bool = True,
+            **kwargs):
+        """
+        This is NOT used when restoring a model from the trainer, but still practical in other cases!
+        """
+        if map_location is not None:
+            checkpoint = pl_load(checkpoint_path, map_location=map_location)
+        else:
+            checkpoint = pl_load(checkpoint_path, map_location=lambda storage, loc: storage)
+
+        hparams = torch.load(os.path.join(os.path.dirname(checkpoint_path), "hparams.pt"))
+        checkpoint[cls.CHECKPOINT_HYPER_PARAMS_KEY] = hparams
+        checkpoint[cls.CHECKPOINT_HYPER_PARAMS_KEY].update(kwargs)
+        model = cls._load_model_state(checkpoint, strict=strict, **kwargs)
+        return model
+
+    def on_save_checkpoint(self, checkpoint):
+        ckpt_callbacks = [cb for cb in self.trainer.callbacks
+                          if issubclass(type(cb), ModelCheckpoint)]
+        if any(ckpt_callbacks):
+            path = ckpt_callbacks[0].dirpath
+        else:
+            path = self.trainer.default_root_dir
+
+        # overwrite previous optim state
+        torch.save({"optimizer_states": checkpoint["optimizer_states"],
+                    "lr_schedulers": checkpoint["lr_schedulers"]
+                    }, os.path.join(path, "last_optim_state.ckpt"))
+
+        # pop optim items from checkpoint
+        checkpoint.pop("optimizer_states")
+        checkpoint.pop("lr_schedulers")
+
+        # save some trainer props
+        if not os.path.exists(os.path.join(path, "trainer_props.pt")):
+            trainer_props = {}
+            for prop in [
+                "max_epochs",
+                "check_val_every_n_epoch",
+                "reload_dataloaders_every_epoch"
+            ]:
+                trainer_props.update({prop: getattr(self.trainer, prop, None)})
+
+            torch.save(trainer_props, os.path.join(path, "trainer_props.pt"))
+
+        return checkpoint
+
+    def on_load_checkpoint(self, checkpoint):
+        # we restore the training state only if the model has a trainer...
+        if getattr(self, "trainer", None) is not None:
+            # ... and the trainer got a ckpt_path to resume from
+            if self.trainer.resume_from_checkpoint is not None:
+
+                ckpt_path = self.trainer.resume_from_checkpoint
+                version_dir = os.path.abspath(os.path.dirname(ckpt_path))
+
+                # make sure we'll find the optim-state or let the user know
+                if "last_optim_state.ckpt" not in os.listdir(version_dir):
+                    raise FileNotFoundError("Expected to find 'last_optim_state.ckpt' in the parent directory "
+                                            "of the model's checkpoint's directory but no such file were found.")
+                # update the checkpoint
+                optim_state = torch.load(os.path.join(version_dir, "last_optim_state.ckpt"))
+                checkpoint["optimizer_states"] = optim_state["optimizer_states"]
+                checkpoint["lr_schedulers"] = optim_state["lr_schedulers"]
+
+            # update the trainer
+            trainer_props = torch.load(os.path.join(version_dir, "trainer_props.pt"))
+            for prop in ["check_val_every_n_epoch",
+                         "reload_dataloaders_every_epoch",
+                         ]:
+                setattr(self.trainer, prop, trainer_props[prop])
+            if checkpoint["epoch"] < trainer_props["max_epochs"]:
+                self.trainer.max_epochs = self.trainer.max_epochs - checkpoint["epoch"]
+            else:
+                self.trainer.max_epochs = self.trainer.max_epochs + checkpoint["epoch"]
+
+        return checkpoint
