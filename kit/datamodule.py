ed38bcc2ceb18720c0bdbdd086e207f230b15e04
antoinedaurat <antoine@antoine>
2020-12-12 06:01:24 +0100

    back up datamodule.py

--- kit/datamodule.py
+++ kit/datamodule.py
@@ -7,17 +7,18 @@ from typing import Sequence, Tuple, Generator, NewType, Union
 from multiprocessing import cpu_count
 
 from mmk.data import FeatureProxy
-from mmk.kit.ds_wrappers import DatasetWrapper, IterableDatasetWrapper, is_map_style_dataset
+from mmk.kit.ds_wrappers import MultiFeatureSet, IterableDatasetWrapper, is_map_style_dataset
 from mmk.data.metadata import Metadata
 
 
 def is_wrapper_ds(dataset):
-    return issubclass(type(dataset), DatasetWrapper) or issubclass(type(dataset), IterableDatasetWrapper)
+    return issubclass(type(dataset), MultiFeatureSet) or issubclass(type(dataset), IterableDatasetWrapper)
 
 
 def zip_stack(batch_lists):
     """
-    returns (stack(feat_1), stack(feat_2)...)
+    @batch_lists: ((feat_1_1, feat_2_1), (feat_1_2, feat_2_2), ...)
+    returns (stack(feat_1[:]), stack(feat_2[:]), ...)
     """
     rv = tuple(torch.stack(x) for x in zip(*batch_lists))
     return rv
@@ -84,176 +85,13 @@ Feature = NewType("Feature",
                         np.ndarray, torch.Tensor, Sequence, Generator])
 
 
-class Dataset(TorchDataset):
-
-    @property
-    def n_features(self):
-        return len(self._shape)
-
-    @property
-    def dims(self):
-        return self._shape
-
-    @property
-    def shape(self):
-        return self._shape
-
-    def size(self):
-        return self._shape
-
-    @property
-    def dtype(self):
-        return self._dtype
-
-    @property
-    def device(self):
-        return self._device
-
-    @property
-    def indices(self):
-        return self._indices
-
-    @property
-    def style(self):
-        return is_map_style_dataset(self)
-
-    @property
-    def attrs(self):
-        """
-        @return: `attrs` : dict of user-specified infos he wants to keep close (e.g. {"sr": 22050, "time-axis": 0})
-        """
-        return self._attrs
-
-    @staticmethod
-    def __new__(cls,
-                sources=None,  # remote locations of data
-                files=None,   # local paths to data
-                objects=None,   # 'datasetable' objects like arrays, tensors or generators
-                datasets=None,  # Datasets we want to wrap or augment
-                wrappers=None,  # wrappers to specify batch creation
-                augmentations=None,   # see the docstring of the `augment` method
-                ):
-        """
-        create a new Dataset either from sources, from files, from python objects or from Datasets.
-        Having instantiation here allows to define subclasses that explicitly deal only with sources or only with files
-        and to instantiate the class most appropriate to the user's input. It's also handy for declaring Datasets
-        on the fly at any step of the workflow described below in the "WORKFLOWS" note.
-        """
-        pass
-
-    def __init__(self):
-        """
-        Here we initialize the instance.
-        """
-        self._source = None
-        self._file = None
-        self._object = None
-        self._wrapper = None
-        self._augmentation = None
-        self._shape = tuple()
-        self._dtype = tuple()
-        self._device = tuple()
-        self._indices = None
-        self._attrs = dict()
-
-    def __call__(self, *args, **kwargs):
-        """
-        Method used to wrap around an other Dataset. Only used in the wrapper classes.
-        """
-        pass
-
-    def __len__(self):
-        pass
-
-    def __getitem__(self, item):
-        pass
-
-    def __iter__(self):
-        pass
-
-    """
-    WORKFLOWS :
-    The Idea behind the four following methods is two-fold :
-    
-    1. we want to enable 2 different workflows. One where we fetch data prior to training and this could mean download 
-    data to disks, or load files as tensors into RAM. The other where we only specify a promise that will be executed 
-    during training ; for instance, we collect file-names and file-lengths prior to training but we load them as tensors
-    into the RAM only when they're requested by the Trainer during training.
-    
-    2. In either workflow, we need to execute tasks that should run in parallel workers/threads and return objects that
-    allow us to keep track of properties of the underlying data, e.g. shapes and lengths. Both requirements being already
-    full-filled by torch's synergy between Dataloaders and Datasets, we only require that the methods executed
-    in parallel (`download_source` and `load_file`) return Dataset objects!
-    Then, we can flexibly define any data-fetching pipeline by simply specifying which steps/methods should be run 
-    prior to training and which steps/methods should be kept as promises. Simple, efficient, and without any 
-    "yet-an-other-API" overhead : at any step, we use the same slight extension of torch's Dataset! 
-    """
-
-    def download_source(self, *args, **kwargs):
-        """method to download a single source.
-        @returns: Dataset object"""
-        pass
-
-    def load_file(self, path):
-        """method to load a single file.
-        Here's the place to define how an audio file should be transformed (e.g. stft) and to extract some features from
-        it (e.g. segments' or filenames' labels).
-        @returns: Dataset object"""
-        pass
-
-    def prepare(self):
-        """perform here the steps that create files prior to training. For instance, download from neptune/youtube or
-        aggregate transformed files into a .h5 DB.
-        Base class implementation of this method creates a simple multiprocessing Pool that calls
-        `download_source` on all the sources if some were passed to the constructor."""
-        return None
-
-    def setup(self):
-        """perform here the steps that create python objects (e.g. Datasets of Tensors, generators...)
-         prior to training.
-         Base class implementation of this method creates a simple multiprocessing Pool that calls
-        `load_file` on all the files created by `prepare()` or passed to the constructor, it then packs the results into
-        an appropriate Dataset class and concatenate all of them into a single Dataset object"""
-        return None
-
-    def wraps_in(self, *wrappers):
-        """add batch definition, augmentations (and transforms?) with wrapper(s)"""
-        pass
-
-    def transform_item(self, item):
-        """callback to do something with the items before they are passed to __getitem__"""
-        pass
-
-    def load(self, **kwargs):
-        """pack self into a batch producer (Dataloader)"""
-        pass
-
-    def to(self, device):
-        """move any underlying tensor to some device"""
-        pass
-
-    def transform(self, function):
-        """transform any underlying tensor"""
-        pass
-
-    def augment(self, *augmentations):
-        """`augmentations` are tuples whose first elements are probabilities (0. < float <= 1) for the augmentations
-        to occur during training and the second elements, functions that takes a batch as single argument
-         and return a transformed batch - the augmentation"""
-        pass
-
-    def random_example(self, n):
-        """get n random examples"""
-        pass
-
-
 class MMKDataModule(pl.LightningDataModule):
 
     def __init__(self,
                  feature: [Feature, Tuple[Feature]],
                  subset_idx: [np.ndarray, torch.Tensor, Metadata, Sequence] = None,
                  malloc_device: [torch.device, str, Sequence] = None,
-                 ds_wrapper: [DatasetWrapper, IterableDatasetWrapper] = None,
+                 ds_wrapper: [MultiFeatureSet, IterableDatasetWrapper] = None,
                  train_val_split=1.,
                  **loader_kwargs,
                  ):
@@ -320,7 +158,7 @@ class MMKDataModule(pl.LightningDataModule):
             dataset = tuple(self._prepare_single_feature(feat, subset_idx, malloc_device) for feat in feature)
         else:
             dataset = self._prepare_single_feature(feature, subset_idx, malloc_device)
-
+        # wrap()
         if ds_wrapper is not None:
             if not isinstance(dataset, tuple):
                 dataset = (dataset,)
@@ -329,7 +167,7 @@ class MMKDataModule(pl.LightningDataModule):
         elif ds_wrapper is None and isinstance(dataset, tuple) and self._is_valid_ds_tuple(dataset):
             # by default, pack multiple features in a default wrapper
             if is_map_style_dataset(dataset[0]):
-                dataset = DatasetWrapper()(*dataset)
+                dataset = MultiFeatureSet()(*dataset)
             else:
                 dataset = IterableDatasetWrapper()(*dataset)
 
@@ -351,9 +189,11 @@ class MMKDataModule(pl.LightningDataModule):
                                  " Please set `malloc_device` to None "
                                  "or pass a `feature` object of one of the following type: "
                                  "mmk.data.FeatureProxy, np.ndarray, torch.Tensor, Sequence")
+            # select(inplace=True) & convert()
             feature = self._feature_to_tensor(feature, subset_idx)
+            # to()
             feature = self._alloc_feature(feature, malloc_device)
-
+        # upgrade()
         dataset = self._feature_to_dataset(feature)
 
         if malloc_device is None and subset_idx is not None:
@@ -361,6 +201,7 @@ class MMKDataModule(pl.LightningDataModule):
                 raise ValueError("Cannot take a subset of an IterableDataset. Please set `subset_idx` to None or "
                                  "pass a feature of another class.")
             subset_idx = MMKDataModule._subset_idx_to_indexing_object(subset_idx)
+            # select(inplace=False)
             dataset = Subset(dataset, subset_idx)
 
         return dataset
@@ -419,7 +260,7 @@ class MMKDataModule(pl.LightningDataModule):
 
     @staticmethod
     def _wrap_dataset(feat, wrapper):
-        if issubclass(wrapper, DatasetWrapper):
+        if issubclass(wrapper, MultiFeatureSet):
             if not isinstance(feat, tuple):
                 feat = (feat,)
             feat = wrapper(*feat)
