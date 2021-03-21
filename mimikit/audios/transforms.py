3b8240ab4d8795bd96f75df8c6e94527159da68c
Bjoern <berlach@ccrma.stanford.edu>
2021-03-21 02:13:25 +0100

    Adaptive sample encoder added

--- mimikit/audios/transforms.py
+++ mimikit/audios/transforms.py
@@ -1,12 +1,28 @@
 import librosa
 from scipy.signal import lfilter
 from ..extract.segment import from_recurrence_matrix
-
+import numpy as np
+from scipy.interpolate import interp1d
 
 N_FFT = 2048
 HOP_LENGTH = 512
 SR = 22050
 Q_LEVELS = 256
+
+
+def _make_unidist_shaper(x):
+    x = np.sort(x, axis=0)
+    indices = _make_unidist_shaper_indices(x)
+    yvals = 2 * indices / indices[-1] - 1.0
+    encode_shaper = interp1d(x[indices], yvals, bounds_error=False)
+    return encode_shaper, list(x[indices]), indices
+
+
+def _make_unidist_shaper_indices(x):
+    ids = np.linspace(0, len(x) - 1, 22).astype(np.int)
+    ids = np.concatenate([ids[:1], [ids[1] + (ids[0] - ids[1]) // 2],
+                          ids[1:-1], [ids[-2] + (ids[-1] - ids[-2]) // 2], ids[-1:]])
+    return ids
 
 
 class SignalTo:
@@ -22,11 +38,28 @@ class SignalTo:
         return abs(S)
 
     @staticmethod
-    def mu_law_compress(y, q_levels=Q_LEVELS):
-        y = librosa.util.normalize(y)
-        qx = librosa.mu_compress(y, q_levels-1, quantize=True)
+    def mu_law_compress(y, q_levels=Q_LEVELS, normalize=True):
+        if normalize:
+            y = librosa.util.normalize(y)
+        qx = librosa.mu_compress(y, q_levels - 1, quantize=True)
         qx = qx + q_levels // 2
         return qx
+
+    @staticmethod
+    def pcm_unsigned(y, q_levels=Q_LEVELS, normalize=True):
+        if normalize:
+            y = librosa.util.normalize(y)
+        qx = ((y + 1.0) * (q_levels - 1)).astype(np.int)
+        return qx
+
+    @staticmethod
+    def adapted_uniform(y, q_levels=Q_LEVELS, normalize=True):
+        if normalize:
+            y = librosa.util.normalize(y)
+        encode_shaper, shaper_vals, shaper_indices = _make_unidist_shaper(y)
+        y = encode_shaper(y)
+        qx = ((y + 1.0) * 0.5 * (q_levels - 1)).astype(np.int)
+        return qx, (shaper_vals, shaper_indices)
 
 
 class SignalFrom:
@@ -49,7 +82,7 @@ def emphasize(y, emphasis):
 
 
 def deemphasize(y, emphasis):
-    return lfilter([1-emphasis], [1, -emphasis], y)
+    return lfilter([1 - emphasis], [1, -emphasis], y)
 
 
 class MagSpecTo:
