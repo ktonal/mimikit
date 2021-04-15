c193074ca2739b651411fe3d0c9d2edb464566bd
antoinedaurat <antoinedaurat@gmail.com>
2021-04-15 07:51:04 +0200

    wip seq2seq lstm

--- mimikit/kit/networks/parametrized_gaussian.py
+++ mimikit/kit/networks/parametrized_gaussian.py
@@ -1,19 +1,25 @@
 import torch
 from torch import nn as nn
+from typing import Optional
+from dataclasses import dataclass
 
 
+@dataclass(init=True, repr=False, eq=False, frozen=False, unsafe_hash=True)
 class ParametrizedGaussian(nn.Module):
     """
     Parametrized Gaussian (often found in variational auto-encoders)
     """
+    input_dim: int
+    z_dim: int
+    bias: Optional[bool] = False
+    pre_activation: Optional[nn.Module] = nn.Identity()
+    return_params: Optional[bool] = True
 
-    def __init__(self, input_dim: int, z_dim: int, pre_activation=nn.Identity(), return_params=True, bias=False):
-        super(ParametrizedGaussian, self).__init__()
-        self.fc1 = nn.Linear(input_dim, z_dim, bias=bias)
-        self.fc2 = nn.Linear(input_dim, z_dim, bias=bias)
-        self.z_dim = z_dim
-        self.pre_act = pre_activation if pre_activation is not None else lambda x: x
-        self.return_params = return_params
+    def __postinit__(self):
+        nn.Module.__init__(self)
+        self.fc1 = nn.Linear(self.input_dim, self.z_dim, bias=self.bias)
+        self.fc2 = nn.Linear(self.input_dim, self.z_dim, bias=self.bias)
+        self.pre_act = self.pre_activation if self.pre_activation is not None else lambda x: x
 
     def forward(self, h):
         mu, logvar = self.fc1(self.pre_act(h)), self.fc2(self.pre_act(h))
--- mimikit/kit/networks/seq2seq_lstms.py
+++ mimikit/kit/networks/seq2seq_lstms.py
@@ -1,33 +1,57 @@
 import torch
 import torch.nn as nn
+from typing import Optional
+from dataclasses import dataclass
+
 from mimikit.kit.networks.parametrized_gaussian import ParametrizedGaussian
 
 
+@dataclass(init=True, repr=False, eq=False, frozen=False, unsafe_hash=True)
 class EncoderLSTM(nn.Module):
-    def __init__(self, input_d, model_dim, num_layers, bottleneck="add", n_fc=1):
-        super(EncoderLSTM, self).__init__()
-        self.bottleneck = bottleneck
-        self.dim = model_dim
-        self.lstm = nn.LSTM(input_d, self.dim if bottleneck == "add" else self.dim // 2, bias=False,
-                            num_layers=num_layers, batch_first=True, bidirectional=True)
-        self.fc = nn.Sequential(
-            *[nn.Sequential(nn.Linear(self.dim, self.dim), nn.Tanh()) for _ in range(n_fc - 1)],
-            nn.Linear(self.dim, self.dim, bias=False),  # NO ACTIVATION !
-        )
-#         for name, p in dict(self.lstm.named_parameters()).items():
-#             if "weight" in name:
-#                 torch.nn.utils.weight_norm(self.lstm, name)
+    input_d: int
+    model_dim: int
+    num_layers: int
+    n_lstm: Optional[int] = 1
+    bottleneck: Optional[str] = "add"
+    n_fc: Optional[int] = 1
+    bias: Optional[bool] = False
+    weight_norm: Optional[bool] = False
 
-    def forward(self, x, hiddens=None, cells=None):
-        if hiddens is None or cells is None:
-            states, (hiddens, cells) = self.lstm(x)
+    def __postinit__(self):
+        nn.Module.__init__(self)
+        self.lstms = nn.ModuleList([
+            nn.LSTM(self.input_d if i == 0 else self.model_dim,
+                    self.model_dim if self.bottleneck == "add" else self.model_dim // 2,
+                    bias=self.bias,
+                    num_layers=self.num_layers,
+                    batch_first=True, bidirectional=True)
+            for i in range(self.n_lstm)
+        ])
+        self.fc = nn.Sequential(
+            *[nn.Sequential(nn.Linear(self.model_dim, self.model_dim), nn.Tanh()) for _ in range(self.n_fc - 1)],
+            nn.Linear(self.model_dim, self.model_dim, bias=False),  # NO ACTIVATION !
+        )
+        if self.weight_norm:
+            for name, p in dict(self.lstms.named_parameters()).items():
+                if "weight" in name:
+                    torch.nn.utils.weight_norm(self.lstms, name)
+
+    def forward(self, x, h0=None, c0=None):
+        if h0 is None or c0 is None:
+            hidden = tuple()
         else:
-            states, (hiddens, cells) = self.lstm(x, (hiddens, cells))
-        states = self.first_and_last_states(states)
-        return self.fc(states), (hiddens, cells)
+            hidden = (h0, c0)
+        ht, ct = None, None
+        for i, lstm in enumerate(self.lstms):
+            out, (ht, ct) = lstm(x, *hidden)
+            # sum forward and backward nets
+            out = out.view(*out.size()[:-1], self.model_dim, 2).sum(dim=-1)
+            # take residuals AFTER the first lstm
+            x = out if i == 0 else x + out
+        states = self.first_and_last_states(x)
+        return self.fc(states), (ht, ct)
 
     def first_and_last_states(self, sequence):
-        sequence = sequence.view(*sequence.size()[:-1], self.dim, 2).sum(dim=-1)
         first_states = sequence[:, 0, :]
         last_states = sequence[:, -1, :]
         if self.bottleneck == "add":
@@ -36,37 +60,43 @@ class EncoderLSTM(nn.Module):
             return torch.cat((first_states, last_states), dim=-1)
 
 
+@dataclass(init=True, repr=False, eq=False, frozen=False, unsafe_hash=True)
 class DecoderLSTM(nn.Module):
-    def __init__(self, model_dim, num_layers, bottleneck="add"):
-        super(DecoderLSTM, self).__init__()
-        self.dim = model_dim
-        self.lstm1 = nn.LSTM(self.dim, self.dim if bottleneck == "add" else self.dim // 2, bias=False,
-                             num_layers=num_layers, batch_first=True, bidirectional=True)
-        self.lstm2 = nn.LSTM(self.dim, self.dim if bottleneck == "add" else self.dim // 2, bias=False,
-                             num_layers=num_layers, batch_first=True, bidirectional=True)
-        for name, p in dict(self.lstm1.named_parameters()).items():
-            if "weight" in name:
-                torch.nn.utils.weight_norm(self.lstm1, name)
-#         for name, p in dict(self.lstm2.named_parameters()).items():
-#             if "weight" in name:
-#                 torch.nn.utils.weight_norm(self.lstm2, name)
+    model_dim: int
+    num_layers: int
+    bottleneck: Optional[str] = "add"
+    bias: Optional[bool] = False
+    weight_norm: Optional[tuple] = (False, False)
+
+    def __postinit__(self):
+        nn.Module.__init__(self)
+        self.lstm1 = nn.LSTM(self.model_dim, self.model_dim if self.bottleneck == "add" else self.model_dim // 2,
+                             bias=self.bias,
+                             num_layers=self.num_layers, batch_first=True, bidirectional=True)
+        self.lstm2 = nn.LSTM(self.model_dim, self.model_dim if self.bottleneck == "add" else self.model_dim // 2,
+                             bias=self.bias,
+                             num_layers=self.num_layers, batch_first=True, bidirectional=True)
+        for lstm, wn in zip([self.lstm1, self.lstm2], self.weight_norm):
+            if wn:
+                for name, p in dict(lstm.named_parameters()).items():
+                    if "weight" in name:
+                        torch.nn.utils.weight_norm(lstm, name)
 
     def forward(self, x, hiddens, cells):
         if hiddens is None or cells is None:
             output, (_, _) = self.lstm1(x)
         else:
-            # decoder does get hidden states from encoder !
+            # ALL decoders get hidden states from encoder
             output, (_, _) = self.lstm1(x, (hiddens, cells))
         # sum forward and backward nets
-        output = output.view(*output.size()[:-1], self.dim, 2).sum(dim=-1)
+        output = output.view(*output.size()[:-1], self.model_dim, 2).sum(dim=-1)
 
         if hiddens is None or cells is None:
             output2, (hiddens, cells) = self.lstm2(output)
         else:
-            # V1 residual does get hidden states from encoder !
             output2, (hiddens, cells) = self.lstm2(output, (hiddens, cells))
         # sum forward and backward nets
-        output2 = output2.view(*output2.size()[:-1], self.dim, 2).sum(dim=-1)
+        output2 = output2.view(*output2.size()[:-1], self.model_dim, 2).sum(dim=-1)
 
         # sum the outputs
         return output + output2, (hiddens, cells)
